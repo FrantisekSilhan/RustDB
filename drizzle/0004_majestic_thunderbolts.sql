66cfd571a5240aa1ddcc89daa3a07720478a9905
František Šilhán <frantisek@slhn.cz>
2025-05-23 13:25:31 +0200

    feat: remove useless primary keys

---
+++ drizzle/0004_majestic_thunderbolts.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "buy_orders" DROP COLUMN "id";--> statement-breakpoint
+ALTER TABLE "buy_order_graphs" DROP COLUMN "id";--> statement-breakpoint
+ALTER TABLE "sell_orders" DROP COLUMN "id";--> statement-breakpoint
+ALTER TABLE "sell_order_graphs" DROP COLUMN "id";
